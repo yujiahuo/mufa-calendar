3d66971e9b6c8e465bd6b915dd0a60de90f7a25c
yujiahuo <yujiahuo7@gmail.com>
2026-06-15 22:52:11 -0500

    Create script to write all the sharable calendar links to a spreadsheet

---
+++ writeCalendarLinks.js
@@ -0,0 +1,46 @@
+function writeCalendarLinks() {
+  const calendarInfo = getMUFACalendarNamesAndLinks();
+  const spreadsheet = SpreadsheetApp.create("MUFA calendar links");
+  addRows(spreadsheet, calendarInfo);
+  console.log("Ok I did it");
+}
+
+function getMUFACalendarNamesAndLinks() {
+  const allCalendars = CalendarApp.getAllCalendars();
+  const calendarInfo = [];
+
+  for (const calendar of allCalendars) {
+    const calendarName = calendar.getName();
+    if (calendarName.includes("MUFA")) {
+      const calendarId = calendar.getId();
+      const encodedId = encodeURIComponent(calendarId);
+      const url = "https://calendar.google.com/calendar/u/0?cid=" + encodedId;
+      calendarInfo.push({
+        name: calendarName,
+        url: url,
+      });
+    }
+  }
+
+  return calendarInfo;
+}
+
+function addRows(spreadsheet, calendarInfo) {
+  const sheet = spreadsheet.getSheets()[0];
+
+  for (let i = 0; i < calendarInfo.length; i++) {
+    const cellContent = getCellContent(
+      calendarInfo[i]?.name,
+      calendarInfo[i]?.url
+    );
+    const targetRange = sheet.getRange("A" + (i + 1));
+    targetRange.setRichTextValue(cellContent);
+  }
+}
+
+function getCellContent(name, url) {
+  return SpreadsheetApp.newRichTextValue()
+    .setText(name)
+    .setLinkUrl(url)
+    .build();
+}
